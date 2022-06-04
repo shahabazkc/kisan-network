36445b72f34522a1485f433213388f36369011ec
Shahabaz Kc <55649768+shahabazkc@users.noreply.github.com>
2022-06-05 03:09:58 +0530

    Updated: Design ui changes

--- client/components/get-contacts.tsx
+++ client/components/get-contacts.tsx
@@ -1,5 +1,10 @@
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
+import styles from '../styles/contact.module.css';
+import Accordion from 'react-bootstrap/Accordion'
+import { FaUserCircle } from 'react-icons/fa';
+import { BiSend } from 'react-icons/bi';
+
 
 export const GetContacts: NextPage = () => {
     const [contacts, setContacts] = useState([]);
@@ -12,20 +17,39 @@ export const GetContacts: NextPage = () => {
     }, [])
 
     return (
-        <div>
-            <h1>Get Contacts</h1>
-            <ul>
-                {
-                    contacts.map((contact: any, index: number) => {
-                        return (
-                            <>
-                                <li key={index}>{contact?.first_name} {contact?.last_name} </li>
-                                <li>{contact.number}</li>
-                            </>
-                        )
-                    })
-                }
-            </ul>
-        </div>
+        <div className="container m-2">
+            <h1>Contacts</h1>
+            <div className={styles.contactList}>
+                <p><b>List of {contacts.length} contacts</b></p>
+                <div className={styles.contactcard}>
+                    <Accordion defaultActiveKey="">
+                        {
+                            contacts.map((contact: any, index: number) => {
+                                return (
+                                    <>
+                                        <Accordion.Item eventKey={index + ""} key={index}>
+                                            <Accordion.Header>
+                                                <div className={styles.card}>
+                                                    <FaUserCircle className={styles.cardIcon} />
+                                                    <span className={styles.cardContent} key={index}>{contact?.first_name} {contact?.last_name} </span>
+                                                </div>
+                                            </Accordion.Header>
+                                            <Accordion.Body>
+                                                <b>
+                                                    First Name: {contact?.first_name}<br />
+                                                    Last Name: {contact.last_name}<br />
+                                                    Number: {contact.number}<br />
+                                                    <button className='btn btn-md btn-dark'>Send Message <BiSend /></button>
+                                                </b>
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                    </>
+                                )
+                            })
+                        }
+                    </Accordion>
+                </div>
+            </div>
+        </div >
     );
 }
